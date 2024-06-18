f2ee81b2fb217580edb479780f639f6bcf35ff35
alysson912 <almenezes912@gmail.com>
2024-06-18 18:57:32 -0300

    START PROJECT

---
+++ swftUI_FirebaseProject/swftUI_FirebaseProject/swftUI_FirebaseProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  swftUI_FirebaseProjectApp.swift
+//  swftUI_FirebaseProject
+//
+//  Created by Alysson Menezes on 18/06/24.
+//
+
+import SwiftUI
+
+@main
+struct swftUI_FirebaseProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
