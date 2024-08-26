a760359eee0ce617845a8ab6cf727b06fc65e934
alysson912 <almenezes912@gmail.com>
2024-08-26 14:55:46 -0300

    Firebase Crashlytics Tutorial for iOS: Enhance Stability and Performance

---
+++ swftUI_FirebaseProject/swftUI_FirebaseProject/ManagerSingletons/Firestore/Crashlytics/CrashView.swift
@@ -0,0 +1,18 @@
+//
+//  CrashView.swift
+//  swftUI_FirebaseProject
+//
+//  Created by Alysson Menezes on 14/08/24.
+//
+
+import SwiftUI
+
+struct CrashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CrashView()
+}
